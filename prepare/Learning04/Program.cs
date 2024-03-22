66c5bcedc2f9e291b2fdecf5510b0a5695cdcf93
Gewasher <1010gmoney@gmail.com>
2024-03-21 21:06:50 -0600

    learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+        Assignment assignment = new Assignment("Gabe", "History");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Gabe", "Fractions", "2.5", "3-11 odd");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomework());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Gabe", "History", "The History of the Thingy");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
